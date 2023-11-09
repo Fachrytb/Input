5d6f11cbeae3a986ccc68bb64542c677b2ec9100
tubagus.fakhridhiya@gmail.com <tubagus.fakhridhiya@gmail.com>
2023-11-09 19:32:35 +0700

    alaj

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDE9RCp2gUIyOG8hUEv5un0JlXqHWowkA",
+  authDomain: "crud-6df8e.firebaseapp.com",
+  projectId: "crud-6df8e",
+  storageBucket: "crud-6df8e.appspot.com",
+  messagingSenderId: "22846946310",
+  appId: "1:22846946310:web:f393dbd1ff1ad9d61f1787"
+};
+
+  
